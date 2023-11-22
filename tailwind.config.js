60770f1107748e174e6e6f64ab2511eb6469c4af
Ntim Yeboah <lylegendary@gmail.com>
2023-11-22 15:09:51 +0000

    Implement about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['index.html', 'page-details.html'],
+  content: [
+    '.html',
+    'page-details.html',
+    'about.html'
+  ],
   theme: {
     extend: {},
   },
